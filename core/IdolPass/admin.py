676a6f4fad1bac4e1917bd2e013b38bec1bd2e99
AndresBerillo <andresberillo.01@gmail.com>
2024-12-03 15:53:04 -0300

    Proyecto Terminado.V2 - Correccion Models

--- core/IdolPass/admin.py
+++ core/IdolPass/admin.py
@@ -1,8 +1,7 @@
 # core/IdolPass/admin.py
 from django.contrib import admin
-from .models import Concert, Ticket, Transaction
+from .models import Concert, Ticket
 
 # Registrar los modelos para que aparezcan en el panel de administración
 admin.site.register(Concert)
 admin.site.register(Ticket)
-admin.site.register(Transaction)
--- core/IdolPass/models.py
+++ core/IdolPass/models.py
@@ -43,13 +43,3 @@ class ShoppingCart(models.Model):
     def __str__(self):
         return f"Carrito de {self.user.username}"
 
-
-class Transaction(models.Model):
-    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
-    buyer = models.ForeignKey(User, on_delete=models.CASCADE)
-    seller = models.ForeignKey(User, related_name='sold_tickets', on_delete=models.CASCADE)
-    date = models.DateTimeField(auto_now_add=True)
-    amount = models.DecimalField(max_digits=8, decimal_places=2)
-
-    def __str__(self):
-        return f"Transaction {self.id} - Ticket {self.ticket.seat_number}"
